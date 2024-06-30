5ccaf8c46c6eb632c7fa33b1a9229188ba3be5f9
Nicholas J. Michalek <djphazer@gmail.com>
2024-06-30 01:32:19 -0400

    pld-rebase2: Adapting Apps (resolved)

--- software/src/APP_QUADRANTS.h
+++ software/src/APP_QUADRANTS.h
@@ -1408,7 +1408,7 @@ static size_t QUADRANTS_restore(const void *storage) {
     return used;
 }
 
-void FASTRUN QUADRANTS_isr() {
+void FASTRUN QUADRANTS_process(OC::IOFrame *) {
     quad_manager.BaseController();
 }
 
