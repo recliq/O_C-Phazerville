2b56656876f75d05c7801163c0affbb5f28230e1
Nicholas J. Michalek <djphazer@gmail.com>
2024-10-08 18:19:57 -0400

    more merge fixups, TODO's

--- software/src/OC_apps.cpp
+++ software/src/OC_apps.cpp
@@ -887,8 +887,10 @@ bool Ui::ConfirmReset() {
 }
 
 void start_calibration() {
+/* TODO:
   OC::apps::set_current_app(0); // switch to Settings app
   Settings_instance.StartCalibration(); // Set up calibration mode in Settings app
+*/
 }
 
 }; // namespace OC
